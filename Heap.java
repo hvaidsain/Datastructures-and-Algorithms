371082965ad74d884b4a28cc0153714ad3aca13b
Harsh Vaidsain <harsh.vaidsain@gmail.com>
2017-09-16 18:29:38 +0530

    Add files via upload

---
+++ Heap.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.math.*;
+import java.io.*;
+
+class Heap{  
+
+	//Program to build a max from a given unsorted array
+
+
+
+
+	public static void maxHeapify(int a[],int i){
+
+		int l = 2*i+1;
+		int r = 2*i+2;
+
+		int largest = i;
+
+
+		if(l<a.length && a[l]>a[largest]){
+			largest = l;
+		}
+
+		if(r<a.length && a[r]>a[largest]){
+			largest = r;
+		}
+
+		if(a[i] != a[largest]){
+
+			int temp = 0;
+			temp = a[i];
+			a[i] = a[largest];
+			a[largest] = temp;
+			maxHeapify(a,largest);
+		}
+
+
+
+
+	}
+
+
+	public static void buildMaxHeap(int a[]){
+
+		for(int i=(a.length)/2 ; i>=0;i--){
+			maxHeapify(a,i);
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		int a[] = {2,4,6,1,8,5,9,3,7,20,11};
+
+		buildMaxHeap(a);
+
+		//displaying the max heap
+
+		for(int i=0;i<a.length;i++){
+			System.out.print(a[i]+" ");
+		}
+	}
+}
